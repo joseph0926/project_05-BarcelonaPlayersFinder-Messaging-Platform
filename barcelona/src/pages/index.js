049134adb7badf24c24daf929c6876e731295bca
김영훈 <joseph0926@naver.com>
2023-05-19 16:08:28 +0900

    #1 Routing

---
+++ barcelona/src/pages/index.js
@@ -0,0 +1,15 @@
+import PlayerDetail from "./players/PlayerDetail.vue";
+import PlayersList from "./players/PlayersList.vue";
+import PlayerRegistraion from "./players/PlayerRegistration.vue";
+import ContactPlayer from "./requests/ContactPlayer.vue";
+import RequestRecevied from "./requests/RequestRecevied.vue";
+import NotFound from "./NotFound.vue";
+
+export {
+  PlayerDetail,
+  PlayersList,
+  PlayerRegistraion,
+  ContactPlayer,
+  RequestRecevied,
+  NotFound,
+};
--- barcelona/src/router/index.js
+++ barcelona/src/router/index.js
@@ -1,14 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import {
+  ContactPlayer,
+  NotFound,
+  PlayerDetail,
+  PlayerRegistraion,
+  PlayersList,
+  RequestRecevied,
+} from "@/pages/index.js";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/player" },
-    { path: "/player", component: null },
-    { path: "/player/:playerId", component: null, children: [{ path: "contact", component: null }] },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/player", component: PlayersList },
+    {
+      path: "/player/:playerId",
+      component: PlayerDetail,
+      children: [{ path: "contact", component: ContactPlayer }],
+    },
+    { path: "/register", component: PlayerRegistraion },
+    { path: "/requests", component: RequestRecevied },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
