f0aa135ee4ef63784f6a114d05a9c97b0b8a6ac0
김영훈 <joseph0926@naver.com>
2023-05-19 17:44:56 +0900

    #5 Reusable Components

---
+++ barcelona/src/components/ui/index.js
@@ -0,0 +1,5 @@
+import BaseCard from "./BaseCard.vue";
+import BaseButton from "./BaseButton.vue";
+import BaseBadge from "./BaseBadge.vue";
+
+export { BaseBadge, BaseCard, BaseButton };
--- barcelona/src/main.js
+++ barcelona/src/main.js
@@ -3,10 +3,15 @@ import App from "./App.vue";
 
 import router from "./router/index.js";
 import store from "./store/index.js";
+import { BaseBadge, BaseButton, BaseCard } from "./components/ui/index.js";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component("BaseCard", BaseCard);
+app.component("BaseButton", BaseButton);
+app.component("BaseBadge", BaseBadge);
 
 app.mount("#app");
